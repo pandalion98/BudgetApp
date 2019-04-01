48bd3bb7d216dd206af0c9ce4dfb1937c34a4983
pandalion98 <pandalion98@gmail.com>
2019-04-01 16:15:47 +0800

    Fixed typo
    Signed-off-by: pandalion98 <pandalion98@gmail.com>

--- app/src/main/java/ph/kirig/budgetapp/AddAccountActivity.java
+++ app/src/main/java/ph/kirig/budgetapp/AddAccountActivity.java
@@ -9,6 +9,6 @@ public class AddAccountActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_acount);
+        setContentView(R.layout.activity_add_account);
     }
 }
